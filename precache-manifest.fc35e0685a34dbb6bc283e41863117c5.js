9205dd60fe404cab69c82250f978a912c95d525d
Matthew Lu <matthew.lu341@gmail.com>
2023-01-17 20:20:35 -0500

    Updates

---
+++ precache-manifest.fc35e0685a34dbb6bc283e41863117c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38e17f4d534b9fea615e92e6e5939af",
+    "url": "/fyte-client/index.html"
+  },
+  {
+    "revision": "bf425d806ecf0c14596c",
+    "url": "/fyte-client/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "adc347c5566ee65fbd99",
+    "url": "/fyte-client/static/css/main.204a4e01.chunk.css"
+  },
+  {
+    "revision": "bf425d806ecf0c14596c",
+    "url": "/fyte-client/static/js/2.16e03337.chunk.js"
+  },
+  {
+    "revision": "c741a9512219825c71dd1ad1178add62",
+    "url": "/fyte-client/static/js/2.16e03337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc347c5566ee65fbd99",
+    "url": "/fyte-client/static/js/main.167ac09e.chunk.js"
+  },
+  {
+    "revision": "f9923e6be512ea6cd82b",
+    "url": "/fyte-client/static/js/runtime-main.dc6cb333.js"
+  },
+  {
+    "revision": "1979077e5bd56d95c3a1735313633f8b",
+    "url": "/fyte-client/static/media/bg6.1979077e.jpg"
+  },
+  {
+    "revision": "f5e165686ebfb61df1db5b06be38c26d",
+    "url": "/fyte-client/static/media/ph.f5e16568.jpg"
+  }
+]);
